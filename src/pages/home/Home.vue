74c689f78efd1c66b381d380627813c90bdca7a2
logic129 <15691755538@163.com>
2019-01-20 17:14:59 +0800

    finish

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@
   import HomeRecommend from './components/Recommend.vue'
   import HomeWeekend from './components/Weekend.vue'
   import axios from 'axios'
+  import {mapState} from 'vuex'
   export default {
     name:'Home',
     components:{
@@ -26,15 +27,19 @@
     },
     data(){
       return{
+        lastCity:'',
         swiperList:[],
         iconList:[],
         recommendList:[],
         weekendList:[]
       }
     },
+    computed:{
+      ...mapState(['city'])
+    },
     methods:{
       getHomeInfo:function(){
-        axios.get('/api/index.json').then(this.getHomeInfoSucc)
+        axios.get('/api/index.json?city='+this.city).then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc:function(res){
         res=res.data
@@ -48,7 +53,14 @@
       }
     },
     mounted:function(){
+      this.lastCity=this.city
       this.getHomeInfo()
+    },
+    activated(){
+      if(this.lastCity!==this.city){
+        this.lastCity=this.city
+        this.getHomeInfo()
+      }
     }
   }
 </script>
