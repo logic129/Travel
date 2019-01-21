ae917f8a67525b9c73b88209b65abe867ac2d752
logic129 <15691755538@163.com>
2019-01-21 11:53:10 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header class="hhh"></detail-header>
   </div>
 </template>
 
 <script>
   import DetailBanner from './components/Banner'
+  import DetailHeader from './components/Header'
   export default {
     name:'Detail',
     components:{
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     }
   }
 </script>
 
 <style lang="stylus" scoped>
-
+  .hhh{
+    height:50rem
+  }
 </style>
